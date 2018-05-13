6c0e85b4751b55b6892acc743c095675622cb5dc
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2018-05-13 18:29:03 +0200

    Enable interface in prod

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const context = () => {
   const algoliaClient = algoliasearch(process.env.APP_ID, process.env.API_KEY);
   const algoliaIndex = algoliaClient.initIndex(process.env.INDEX_NAME);
 
-  console.info(`Instantiated algolia client ${process.env.APP_ID}/${process.env.INDEX_NAME}`);
-
   return { algoliaIndex, algoliaClient };
 };
 
-console.log(process.env);
-new ApolloServer({ typeDefs, resolvers, context })
+new ApolloServer({ typeDefs, resolvers, context, enableIntrospection: true })
   .listen({ port: process.env.PORT })
   .then(
     serverInfo => console.log(`Server started at ${serverInfo.url}`),
